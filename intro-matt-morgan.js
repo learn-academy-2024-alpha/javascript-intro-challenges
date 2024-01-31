7beaba70289370aadb76146e7460629b34386a76
MattBarnabo <mattbarnabo@gmail.com>
2024-01-31 17:05:40 -0600

    adds first half of the answers

---
+++ intro-matt-morgan.js
@@ -0,0 +1,34 @@
+// Write the code that will log the output of 34 added to 71.
+    console.log(34 + 71);
+// Write the code that will log the output of 67 subtracted from 123.
+    console.log(123 - 67);
+// Write the code that will log the output of 56 multiplied by 23.
+    console.log(56 * 23);
+// Write the code that will log the output of 45 divided by 5.
+    console.log(45 / 5)
+// Write the code that will log the output of 5 to the power of 7.
+    console.log(5 ** 7)
+// Write the code that will log the whole number remainder of 33 divided by 6.
+    console.log(33 / 6)
+// Write the code that will log the length of a string containing your name.
+    let player1 = 'Matt'
+        console.log(player1.length)   
+// Write the code that will log whether your string includes the letter "e"?
+    console.log(player1.includes("e"))
+// Write the code that will log the character at the first index of the string.
+    console.log(player1[0])
+// Write the code that will log the string in all uppercase letters.
+    console.log(player1.toUpperCase)
+
+// Write the code that will log theAnswer divided by 2.
+// Write the code that will log the whole number remainder of theAnswer when divided by 3.
+// Write the code that will log the length of theQuestion.
+// Write the code that will log the length of theQuestion divided by 2.
+// Write the code that will log the length of theQuestion added to theAnswer.
+// Write the code that will log the index of the character "f" in the theQuestion.
+// Write the code that will log the index of the second comma in the theQuestion.
+// Write the code that will log the concatenation of the two variables.
+// Write the code that will log the character "l" from theQuestion.
+// Write the code that will extract "the universe" from theQuestion.
+// Write the code that will extract "everything" from theQuestion.
+// Write the code that will log the last character of theQuestion without manually counting the number of characters.
