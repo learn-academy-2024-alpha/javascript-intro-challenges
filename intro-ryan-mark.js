98ae0883d444ad65c580be420f5a53ae90259072
Rlemus93 <rlemus93@gmail.com>
2024-01-31 14:58:58 -0800

    1st 5 questions

---
+++ intro-ryan-mark.js
@@ -0,0 +1,15 @@
+// Write the code that will log the output of 34 added to 71.
+console.log(34 + 71)
+// Write the code that will log the output of 67 subtracted from 123.
+console.log(67 - 123)
+// Write the code that will log the output of 56 multiplied by 23.
+console.log(56 * 23)
+// Write the code that will log the output of 45 divided by 5.
+console.log(45 / 5)
+// Write the code that will log the output of 5 to the power of 7.
+console.log(5 ** 7)
+// Write the code that will log the whole number remainder of 33 divided by 6.
+// Write the code that will log the length of a string containing your name.
+// Write the code that will log whether your string includes the letter "e"?
+// Write the code that will log the character at the first index of the string.
+// Write the code that will log the string in all uppercase letters.
