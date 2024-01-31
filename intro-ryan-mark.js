38613de22a0d20847b73f03fbe3421a248811f88
Rlemus93 <rlemus93@gmail.com>
2024-01-31 15:57:08 -0800

    answered bottom 6 questions

--- intro-ryan-mark.js
+++ intro-ryan-mark.js
@@ -23,16 +23,22 @@ console.log(name.toUpperCase())
 
 // Consider the variables:
 
-// const theQuestion = "life, the universe, and everything"
-// const theAnswer = 42
+const theQuestion = "life, the universe, and everything"
+const theAnswer = 42
 
 
 // Write the code that will log theAnswer divided by 2.
+console.log (theAnswer / 2)
 // Write the code that will log the whole number remainder of theAnswer when divided by 3.
+console.log (theAnswer % 3)
 // Write the code that will log the length of theQuestion.
+console.log(theQuestion.length)
 // Write the code that will log the length of theQuestion divided by 2.
+console.log(theQuestion.length / 2)
 // Write the code that will log the length of theQuestion added to theAnswer.
+console.log(theQuestion.length + theAnswer)
 // Write the code that will log the index of the character "f" in the theQuestion.
+console.log(theQuestion[2])
 // Write the code that will log the index of the second comma in the theQuestion.
 // Write the code that will log the concatenation of the two variables.
 // Write the code that will log the character "l" from theQuestion.
