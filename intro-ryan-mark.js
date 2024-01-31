c3e13ebcd5b14852348e86ad838857efa5868f40
mark19242 <mark19242@gmail.com>
2024-01-31 15:33:01 -0800

    answered more questions

--- intro-ryan-mark.js
+++ intro-ryan-mark.js
@@ -9,7 +9,33 @@ console.log(45 / 5)
 // Write the code that will log the output of 5 to the power of 7.
 console.log(5 ** 7)
 // Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33 %6)
 // Write the code that will log the length of a string containing your name.
+let name = "mark"
+console.log(name.length)
 // Write the code that will log whether your string includes the letter "e"?
+console.log(name.includes("e"))
 // Write the code that will log the character at the first index of the string.
+console.log(name[0])
 // Write the code that will log the string in all uppercase letters.
+console.log(name.toUpperCase())
+
+
+// Consider the variables:
+
+// const theQuestion = "life, the universe, and everything"
+// const theAnswer = 42
+
+
+// Write the code that will log theAnswer divided by 2.
+// Write the code that will log the whole number remainder of theAnswer when divided by 3.
+// Write the code that will log the length of theQuestion.
+// Write the code that will log the length of theQuestion divided by 2.
+// Write the code that will log the length of theQuestion added to theAnswer.
+// Write the code that will log the index of the character "f" in the theQuestion.
+// Write the code that will log the index of the second comma in the theQuestion.
+// Write the code that will log the concatenation of the two variables.
+// Write the code that will log the character "l" from theQuestion.
+// Write the code that will extract "the universe" from theQuestion.
+// Write the code that will extract "everything" from theQuestion.
+// Write the code that will log the last character of theQuestion without manually counting the number of characters.
