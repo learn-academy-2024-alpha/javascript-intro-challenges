461a2329d728c12e3920190989b397d4fb307f0e
MFDD19D <judas.sanchez1@yahoo.com>
2024-02-01 13:00:27 -0800

    on last question

--- conditionals-matt-ryan-judas.js
+++ conditionals-matt-ryan-judas.js
@@ -26,10 +26,43 @@
 // Write a conditional statement for the following prompts. Make sure you try different options and change the variables to ensure properly working code.
 
 // Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+    const item = 100
+        if (item <= 100){
+            console.log("in budget")
+        }
+        
+
 // Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+        let hungry = true
+            hungry = false 
+
+        if (hungry === true){
+            console.log("eat food")
+        } else { 
+            console.log("keep coding")
+        }
+        
 // Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+        let trafficLight = "green"
+            trafficLight = "yellow"
+        if (trafficLight === "green") {
+            console.log("go");
+        } else if (trafficLight === "yellow"){
+            console.log("slow down");
+        } else if (trafficLight === "red"){
+        console.log("stop");
+        }
+
 // Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+        const firstNum = 30
+        const secondNum = 78
+        if (firstNum < secondNum){
+            console.log(secondNum)
+        } else if (firstNum === secondNum){
+            console.log("numbers are the same")
+        } 
 // Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+    
 // 🏔 Stretch Goals
 // Write a conditional statement for the following prompts.
 
