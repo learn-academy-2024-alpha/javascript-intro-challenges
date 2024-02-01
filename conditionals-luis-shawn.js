1187835635388aec2c302b79739308fdebb4b036
LEARN Academy <learnacademy@macbook-air-2.lan>
2024-02-01 11:11:12 -0800

    updating file

---
+++ conditionals-luis-shawn.js
@@ -0,0 +1,41 @@
+// Copy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt.
+
+// Write the code that will log true or false for the following:
+
+// Is 34 divided by 3 greater than 67 divided by 2?
+var1 = 34/3 
+var2 = 67/2
+if(var1 < var2) {
+    console.log("correct")
+} else{
+    console.log("incorrect")
+}
+
+// Does 5 evaluate to the same as "5"?
+
+// Does 5 strictly equal "5"?
+
+// Does !3 strictly equal 3?
+
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+
+// Does "LEARN" contain the subset "RN"?
+
+// Does "LEARN" contain the subset "rn"?
+// Does "LEARN"[0] strictly equal "l"?
+// Modify the code from the previous question to return true.
+// Write a conditional statement for the following prompts. Make sure you try different options and change the variables to ensure properly working code.
+
+// Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+// Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+// Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+// 🏔 Stretch Goals
+// Write a conditional statement for the following prompts.
+
+// Write a statement that takes a variable of a grade percentage and logs the letter grade for that percentage, if the grade is 100% log "perfect score", if the grade is zero log "no grade available."
+// Write a statement that takes a variable of a boolean, number, or string data type and logs the data type of the variable. HINT: Check out the JavaScript typeof operator.
+// Create a password checker using a single conditional statement. If a user inputs a password with 12 or more characters AND the password includes !, then log "That is a mighty strong password!" If the user’s password is 8 or more characters OR includes !, then log "That password is strong enough." Log "That is not a valid password." for every other input.
