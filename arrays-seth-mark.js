2e04d768c307c76ffdf5937ab4db96d9ad5fd492
sethcrist <sethcrist50@gmail.com>
2024-02-01 15:39:25 -0800

    answered some questions

---
+++ arrays-seth-mark.js
@@ -0,0 +1,32 @@
+var groceryList = ["chips", "dip", "cookies"]
+// Write the code that will add "soda" to the end of the original array.
+    // console.log(groceryList.push("soda"));
+    // console.log(groceryList);
+
+// Write the code that will add "granola" to the end of the array without altering the original array.
+    // groceryList[4] = "granola"
+    // console.log(groceryList);
+
+// Write the code that will return a subset of the array containing only "chips" and "dip".
+    // console.log(groceryList.slice(0, 2));
+
+// Write the code that will add "beans" to the "chips" and "dip" array.
+    // beansArray = "beans"
+    // console.log(groceryList.slice(0, 2) + " " + beansArray);
+
+var numbers = [2, 4, 6, 8, 10]
+// Write the code that will add the number 0 to the beginning of the array.
+    // console.log(numbers.unshift(0), (numbers));
+
+// Write the code that will add the number 12 to the end of the array.
+    // console.log(numbers.push(12), (numbers));
+    
+// Write the code that will remove the first number from the array.
+    // console.log(numbers.shift(), (numbers));
+
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+    // let num0 = 0
+    // console.log(num0 + "," + numbers);
+    // console.log(numbers);
+
+var numSet = [2, 13, 6, 8, 4, 2]
