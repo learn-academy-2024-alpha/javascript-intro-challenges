285f490cfd3999b983068505c288ac5b944ba85c
sethcrist <sethcrist50@gmail.com>
2024-02-01 11:43:01 -0800

    finished the exercise

--- conditionals-mark-seth.js
+++ conditionals-mark-seth.js
@@ -18,3 +18,56 @@ console.log("LEARN".includes("rn")); //false
 console.log("LEARN"[0] === "l"); //false
 console.log("LEARN"[0] === "L"); //true
 // Modify the code from the previous question to return true.
+
+// Write a conditional statement for the following prompts. 
+//Make sure you try different options and change the variables to ensure 
+//properly working code.
+
+// Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+let itemPrice = 80;
+if (itemPrice <= 100) {
+    console.log("in budget");
+} else {
+    console.log("out of budget");
+}
+
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+let hungry = false
+if (hungry) {
+    console.log("eat food");
+} else {
+    console.log("keep coding");
+}
+
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+let trafficLight = "green"
+if (trafficLight === "green") {
+    console.log("go");
+} else if (trafficLight === "yellow") {
+    console.log("slow down");
+} else if (trafficLight === "red"){
+    console.log("stop");
+} else {
+    console.log("not a trafic color");
+}
+
+// Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+let num1 = 30
+let num2 = 30
+if (num1 > num2) {
+    console.log(num1 + " is larger");
+} else if (num2 > num1) {
+    console.log(num2 + " is larger");
+} else {
+    console.log("the numbers are the same");
+}
+
+// Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+let num = 2
+if (num === 0) {
+    console.log("this is 0");
+} else if ( num % 2 === 0) {
+    console.log("this is even");
+} else {
+    console.log("this is odd");
+}
