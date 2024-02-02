c02d940478790e0b676a89dbb907dc66a2902fba
Rlemus93 <rlemus93@gmail.com>
2024-02-02 13:39:45 -0800

    answered 3rd question

--- loops-ryan-amir.js
+++ loops-ryan-amir.js
@@ -12,6 +12,10 @@ for(let i=0; i<secNum.length; i+=2){
     console.log("index:", i, "value", secNum[i])
 }
 // Create a for loop that logs the result of each number from 1 - 20 tripled.
+let thirdNum =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+for(let i=0; i<thirdNum.length; i++){
+    let result = thirdNum[i] * 3
+    console.log(`tripled is: ${result}`)
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
 // Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
 // Consider this variable:
