497fd482ca9711bd513411c8d0258a6b41309964
LEARN Academy <learnacademy@macbook-air-2.lan>
2024-02-01 16:49:12 -0800

    files up to date

--- arrays-luis-shawn.js
+++ arrays-luis-shawn.js
@@ -28,8 +28,17 @@
 
 // Consider the variable:
 
-//var numbers = [2, 4, 6, 8, 10]
+var numbers = [2, 4, 6, 8, 10]
+
 // Write the code that will add the number 0 to the beginning of the array.
+var news = numbers.unshift(0)
+console.log(news)
+console.log(numbers)
+console.log(numbers.shift())
+console.log(numbers)
+console.log(numbers.push(12))
+console.log(numbers)
+console.log(numbers.shift())
 
 // Write the code that will add the number 12 to the end of the array.
 // Write the code that will remove the first number from the array.
