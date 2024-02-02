30d605e580784f4b68836f5411589e0b74642b98
LEARN Academy <learnacademy@macbook-air-2.lan>
2024-02-02 11:53:17 -0800

    added more answers

--- loops-mark-morgan-luis.js
+++ loops-mark-morgan-luis.js
@@ -14,12 +14,59 @@
 //     console.log(tripled);
 // }
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
-for (let i=1; i<=20; i++){
-    if(i % 2 == 0) {
-        console.log(i);
-    } else {
-        console.log("ODD");
-    }
-}
+// for (let i=1; i<=20; i++){
+//     if(i % 2 == 0) {
+//         console.log(i);
+//     } else {
+//         console.log("ODD");
+//     }
+// }
 
 // Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
+
+//const nums = [3, 57, -9, 20, 67]
+// Create the code that will log the largest number from the array.
+// var biggestNumber = 0
+// for(let i=0; i < nums.length; i++){
+//     if(nums[i]> biggestNumber){
+//         biggestNumber= nums[i]
+//     }
+// }
+//     console.log(biggestNumber)
+
+// Create the code that will log the smallest number from the array.
+// var smallestNumber = 0
+//  for(let i=0; i < nums.length; i++){
+//      if(nums[i]< smallestNumber){
+//          smallestNumber= nums[i]
+//      }
+//  }
+//      console.log(smallestNumber)
+
+// Create the code that will log the remainder of each number when divided by 2.
+// for(let i=0; i < nums.length; i++){
+// var remainder = nums[i] % 2
+// console.log(remainder)
+// }
+// Expected output: 1, 1, -1, 0, 1
+// Consider this variable:
+
+const myString = "learn student"
+// Create the code that will log the number of times the letter "e" occurs in the string.
+// var counter = 0
+// var stringArray = myString.split("")
+//     for(let i = 0; i < myString.length; i++){
+//         if(stringArray[i]=== "e"){
+//             counter++
+//         }
+//     }
+//         console.log(counter)
+    
+
+// Create the code that will log every other character in the string.
+
+// var stringArray =myString.split("")
+//     for(let i = 0; i < stringArray.length; i+=2){
+//         console.log(stringArray[i])
+//     }
+
