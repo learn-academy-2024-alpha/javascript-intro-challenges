b033fd299acb1b1ba29fceffcf87abd347a26a8e
Rashadjaxon <amirjacksonscholar@gmail.com>
2024-02-01 16:09:45 -0800

    what a wonderful world

--- arrays-morgan-amir.js
+++ arrays-morgan-amir.js
@@ -45,14 +45,15 @@ var numSet = [2, 13, 6, 8, 4, 2]
 
 // Write the code that finds the index of the first appearance of the number 2.
 
-
+console.log(numSet.indexOf(2))
 
 
 // Write the code that finds the index of the last appearance of the number 2.
 
-
+console.log(numSet.lastIndexOf(2))
 
 
 
 // Write the code that returns the number at the third index.
 
+console.log(numSet[2])
