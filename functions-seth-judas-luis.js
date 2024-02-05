4965d545b24aadb8440be12c19862a10bddf4e27
LEARN Academy <learnacademy@macbook-air-2.lan>
2024-02-05 13:34:02 -0800

    first five questions answered

---
+++ functions-seth-judas-luis.js
@@ -0,0 +1,32 @@
+//Write a function named marco that returns "polo".
+// const marco =() =>{
+//     return "polo" 
+// }
+//      console.log(marco())
+//Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+// const greeting =(name) =>{
+//     return `welcome, ${name}`
+// }
+
+//     console.log(greeting("luis"))
+//Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+// const oddOrEven =(number) =>{
+//     if(number % 2 ===0){
+//         return `the number ${number} is even`
+// }  else{
+//     return `the number ${number} is odd`
+// }  
+// }
+//     console.log(oddOrEven(7))
+
+//Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+// const triple = (number) =>{
+//     return `${number} * 3 is ${number * 3}:`
+// } 
+//     console.log(triple(5))
+
+//Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
+// const multiply = (number1,number2) =>{
+//     return `${number1} * ${number2}=${number1 * number2}`
+// } 
+//     console.log(multiply(4,7));
