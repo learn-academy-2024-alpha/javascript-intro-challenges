6f42a65389924d89457ed9468207da12644bceee
smorgannicole <morgann.smithh@yahoo.com>
2024-02-05 14:25:38 -0800

    finished 2nd stretch goal

--- functions-ryan-morgan.js
+++ functions-ryan-morgan.js
@@ -188,3 +188,42 @@ console.log(greeting(person))
 
 // Write a function named pluralizer that takes a number and a singular noun as arguments and returns the number and pluralized form of the noun, if necessary.
 // Enhance your function so it can handle a few collective nouns like "sheep", "goose", "child", "person" and "species".
+
+// input: takes 2 variables. 1st a number. 2nd a string (singular noun)
+// output: returns number and string (pluralized noun)
+// logic: conditional, built in method
+// pluralizer(5, "cat")
+// // output: "5 cats"
+
+// pluralizer(1, "dog")
+// // output: "1 dog"
+
+// pluralizer(9, "dog")
+// // output: "9 dogs"
+
+// pluralizer(1, "people")
+// // output: "1 person"
+
+// pluralizer(3, "people")
+// // output: "3 people"
+
+const pluralizer = (number, singularNoun) => {
+    if (singularNoun === "sheep") {
+        return `${number} ${singularNoun}` 
+    } else if (singularNoun === "goose") {
+        return `${number} ${singularNoun}`
+    } else if (singularNoun === "child") {
+        return `${number} ${singularNoun}`
+    } else if (singularNoun === "person") {
+        return `${number} ${singularNoun}`
+    } else if (singularNoun === "species") {
+        return `${number} ${singularNoun}`
+    } else if (number === 1) {
+        return `${number} ${singularNoun}`
+    } else if (number > 1) {
+        return `${number} ${singularNoun}s`
+    }
+}
+let number1 = 1
+let singularNoun1 = "dog"
+console.log(pluralizer(number1, singularNoun1))
